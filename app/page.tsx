821e18b25efb829c6164482bbc70542f7b1fd074
vaibhav212303 <vaibhavp@testrig.co.in>
2025-11-16 19:36:53 +0530

    added first version of app

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <div>
-        <main>
-          <p className="text-3xl font-medium text-sky-700">Hello Dappled</p>
-        </main>
-        <Button>
-          Click me
-        </Button>
-      </div>
-    </div>
-  );
+const TestPage = () => {
+  return (<div>
+    <p>Hellow world</p>
+  </div>);
 }
+
+export default TestPage;
