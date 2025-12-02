b97172d778ba05ba141fce7e98c85f212227be1b
vaibhav212303 <vaibhavp@testrig.co.in>
2025-12-02 09:20:10 +0530

    added the sidebar

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 // import { useRouter } from "next/router";
 
 interface SidebarItemProps {
@@ -10,13 +11,44 @@ interface SidebarItemProps {
     href: string;
 }
 
+
+
 export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
     const pathname = usePathname();
-    // const router = useRouter();
+    const router = useRouter();
     const isActive = (pathname === "/" && href === "/") || (pathname === href || pathname?.startsWith(`${href}/`));
-    return (
-        <div>
 
-        </div>
+    const onClick = () => {
+        router.push(href);
+    }
+
+    return (
+        <button
+            onClick={onClick}
+            type="button"
+            className={
+                cn(
+                    "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-gray-600 hover:bg-grey-300/20",
+                    isActive && "text-grey-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+                )
+            }
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon
+                    size={22}
+                    className={
+                        cn(
+                            "text-slate-500", isActive && "text-sky-700"
+                        )
+                    }
+                />
+                {label}
+            </div>
+            <div
+            className={cn(
+                "ml-auto opacity-0 border-2 border-gray-700 h-full transition-all", isActive && "opacity-100"
+            )}
+            />
+        </button>
     );
 }
--- app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-    return (<div>
-        this is the landing page
-    </div>);
-}
-
-export default Home;
