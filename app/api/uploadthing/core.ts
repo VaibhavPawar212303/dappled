a344b054a3e6d16a867d3ed203968eb4f21fbb74
vaibhav212303 <vaibhavp@testrig.co.in>
2025-12-06 16:15:53 +0530

    removed file uplaod flow

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,23 +1,23 @@
-import { auth } from "@clerk/nextjs/server";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+// import { auth } from "@clerk/nextjs/server";
+// import { createUploadthing, type FileRouter } from "uploadthing/next";
 
-const f = createUploadthing();
+// const f = createUploadthing();
 
-const handleAuth = async () => {
-    const { userId } = await auth();
-    if (!userId) throw new Error("Unauthorized");
-    return { userId };
-}
+// const handleAuth = async () => {
+//     const { userId } = await auth();
+//     if (!userId) throw new Error("Unauthorized");
+//     return { userId };
+// }
 
-// FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
-    courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } }).middleware(() => handleAuth())
-        .onUploadComplete(() => { }),
-    courseAttachment: f(["text", "image", "video", "audio", "pdf"]).middleware(() => handleAuth())
-        .onUploadComplete(() => { }),
-    chapterVideo: f({ video: { maxFileSize: "512GB", maxFileCount: 1 } }).middleware(() => handleAuth())
-        .onUploadComplete(() => { }),
+// // FileRouter for your app, can contain multiple FileRoutes
+// export const ourFileRouter = {
+//     courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } }).middleware(() => handleAuth())
+//         .onUploadComplete(() => { }),
+//     courseAttachment: f(["text", "image", "video", "audio", "pdf"]).middleware(() => handleAuth())
+//         .onUploadComplete(() => { }),
+//     chapterVideo: f({ video: { maxFileSize: "512GB", maxFileCount: 1 } }).middleware(() => handleAuth())
+//         .onUploadComplete(() => { }),
 
-} satisfies FileRouter;
+// } satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter;
+// export type OurFileRouter = typeof ourFileRouter;
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,23 +1,23 @@
-"use client";
-import { ourFileRouter } from "@/app/api/uploadthing/core";
-import { UploadDropzone } from "@/lib/uploadthings";
-import toast from "react-hot-toast";
+// "use client";
+// import { ourFileRouter } from "@/app/api/uploadthing/core";
+// import { UploadDropzone } from "@/lib/uploadthings";
+// import toast from "react-hot-toast";
 
-interface FileUploadProps {
-    onChange: (url?: string) => void;
-    endpoint: keyof typeof ourFileRouter;
-}
+// interface FileUploadProps {
+//     onChange: (url?: string) => void;
+//     endpoint: keyof typeof ourFileRouter;
+// }
 
-export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
-    return (
-        <UploadDropzone
-            endpoint={endpoint}
-            onClientUploadComplete={(res) => {
-                onChange(res?.[0].ufsUrl);
-            }}
-            onUploadError={(error: Error) => {
-                toast.error(`${error?.message}`)
-            }}
-        />
-    )
-}
+// export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
+//     return (
+//         <UploadDropzone
+//             endpoint={endpoint}
+//             onClientUploadComplete={(res) => {
+//                 onChange(res?.[0].ufsUrl);
+//             }}
+//             onUploadError={(error: Error) => {
+//                 toast.error(`${error?.message}`)
+//             }}
+//         />
+//     )
+// }
